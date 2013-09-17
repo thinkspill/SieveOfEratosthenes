002c8b0069be1a0ae3e0c032012323daa782d858
Ian Cook <bitbucket@thinkspill.org>
2013-09-17 12:24:27 -0700

    array_fill is much faster than a while loop

--- Sieve.php
+++ Sieve.php
@@ -13,12 +13,17 @@ class Sieve
     private $time = 0.0;
     private $start = 0.0;
     private $end = 0.0;
+    private $fill_start = 0.0;
+    private $fill_end = 0.0;
+    private $fill_time = 0.0;
 
     public function __construct($n)
     {
         $this->start_timing();
         $this->n = $n;
+        $this->start_timing(true);
         $this->create_range($n);
+        $this->end_timing(true);
         foreach ($this->range as $possible_prime => $is_prime) {
             // $this->debug_message("Checking " . $possible_prime);
             if ($this->range[$possible_prime] == 0) {
@@ -39,16 +44,9 @@ class Sieve
      */
     private function create_range($n)
     {
-        $i = 2;
-        while (true) {
-            if ($i == $n + 1) {
-                return;
-            }
-            $this->ops++;
-            $this->range[$i] = 1;
-            $i++;
-        }
+        $this->range = array_fill(2, $n - 1, 1);
     }
+
 
     /**
      * marks all composite numbers of a given prime
@@ -98,15 +96,30 @@ class Sieve
         return $is;
     }
 
-    private function start_timing()
+    private function start_timing($fill = false)
     {
-        $this->start = microtime(true);
+        $field_start = 'start';
+
+        if ($fill) {
+            $field_start = 'fill_start';
+        }
+        $this->$field_start = microtime(true);
+
+
     }
 
-    private function end_timing()
+    private function end_timing($fill = false)
     {
-        $this->end = microtime(true);
-        $this->time = round($this->end - $this->start, 4);
+        $field_start = 'start';
+        $field_end = 'end';
+        $field_time = 'time';
+        if ($fill) {
+            $field_end = 'fill_end';
+            $field_time = 'fill_time';
+        }
+        $this->$field_end = microtime(true);
+        $this->$field_time = round($this->$field_end - $this->$field_start, 4);
+
     }
 
     /**
@@ -124,11 +137,16 @@ class Sieve
     {
         $count_primes = count($this->primes);
         $pps = round(count($this->primes) / $this->time);
+        //        print_r($this->range);
+        //        print_r($this->primes);
         echo "\n";
+        echo "Searching " . $this->n . " numbers.\n";
         echo "Found " . $count_primes . " primes in " . $this->time . " s\n";
         echo $pps . " primes per second in " . $this->ops . " operations (" . round(
                 $this->ops / $this->n
             ) . "*n + n ops)";
+        echo "\nFill time: " . $this->fill_time;
+        echo "\nMemory: " . number_format(memory_get_peak_usage());
         echo "\n";
 
     }
@@ -146,6 +164,6 @@ $s->report();
 $s = new Sieve(10000);
 $s->report();
 
-$s = new Sieve(100000);
+$s = new Sieve(20000);
 $s->report();
 
